ec2d406ff70a4d5ec6d38244b644f115969eea3c
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-01-29 08:29:20 -0500

    Updated from Brython Server: 1/29/2018 8:29:19 AM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,20 @@
+#Ray Tso
+#1/29/18
+#gradeCalculator.py
+
+grade=float(input(' Enter your grade:'))
+
+if grade > 90:
+    print(grade, 'You got a A:')
+
+if grade > 80:
+    print(grade, 'You got a B:')
+
+elif grade >70:
+    print(grade, ' You got a C')
+
+elif grade >70:
+    print(grade, ' You got a D')
+else:
+    print(grade,'You got a F')
 
