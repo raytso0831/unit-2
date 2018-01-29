4bb860c237c8edf43bb8fbe3cba951e9fc461582
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-01-29 12:01:51 -0500

    Updated from Brython Server: 1/29/2018 12:01:50 PM

--- fortuneTeller.py
+++ fortuneTeller.py
@@ -7,27 +7,27 @@ number=int(input('Pick a number from 1-4:'))
 
 
 if colour=='blue' and number==1:
-    print('You will be attacked by a bear:')
+    print('You will be attacked by a bear')
 
 elif colour=='blue' and number==2:
-    print('You will be attacked by a alligator:')
+    print('You will be attacked by a alligator')
 
 elif colour=='blue' and number==3:
-    print('You will be attacked by a shark:')
+    print('You will be attacked by a shark')
 
 elif colour=='blue' and number==4:
-    print('You will be attacked by a tiger:')
+    print('You will be attacked by a tiger')
 
 elif colour=='red' and number==1:
-    print('You will be attacked by a North Korea:')
+    print('You will be attacked by your teacher')
     
 elif colour=='red' and number==2:
-    print('You will be attacked by a hippo:')
+    print('You will be attacked by a hippo')
     
 elif colour=='red' and number==3:
-    print('You will be attacked by a rhino:')
+    print('You will be attacked by a rhino')
     
 else:
-    print('You will be attacked by a lion:')
+    print('You will be attacked by a lion')
     
     
