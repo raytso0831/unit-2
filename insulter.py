3b820c9a757f197d3da210d32e9aecdbea63b1c2
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-01-30 08:39:55 -0500

    Updated from Brython Server: 1/30/2018 8:39:54 AM

--- insulter.py
+++ insulter.py
@@ -4,8 +4,20 @@
 
 from random import randint 
 
-input(' Enter your name')
+input(' Enter your name:')
 
+num=randint(1,5)
 
+if num==1:
+    print( ' “You can lose weight, but you’ll always be ugly.”')
+elif num==2:
+    print(' You’re dumber than snake mittens.')
+    
+elif num==3:
+    print(' You are so stupid')
+elif num==4:
+    print('You are dumb')
+else:
+     print('You onced own a dog that was smarter than you')
 
 
