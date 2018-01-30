0a82106e10b869c90a5d1bfe6bc278a80fb4f09b
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-01-30 08:45:50 -0500

    Updated from Brython Server: 1/30/2018 8:45:49 AM

--- coinFlip.py
+++ coinFlip.py
@@ -1,1 +1,19 @@
+#Ray Tso
+#1/29/18
+#coinFlip.py
 
+from random import randint 
+
+num=randint(1,5)
+
+if num==1:
+    print( num,' It is heads')
+elif num==2:
+    print(num,' It is tails')
+    
+elif num==3:
+    print(num,' It is heads')
+elif num==4:
+    print(num,'It is tails')
+else:
+     print(num,'It is heads')
