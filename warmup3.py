dc1a44385e887d251f83f42fb4122e4d35357654
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-01-31 08:24:47 -0500

    Updated from Brython Server: 1/31/2018 8:24:46 AM

--- warmup3.py
+++ warmup3.py
@@ -1,1 +1,15 @@
+#Ray Tso
+#1/31/18
+#warmup3.py
 
+num=int(input('Enter a integer:'))
+
+if num%3==0 and num%2==0:
+    print(num, ' is positive and divisible by both 2 and 3')
+
+elif num>0 and num%2==0:
+    print(num, ' is divisble by only 2')
+elif num>0 and num%3==0:
+    print(num, ' is divisible by only 3')
+else:
+    print(num, ' is not divisble by 2 or 3')
