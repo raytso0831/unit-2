4252df3719b5f37b4ddd3e63c2ee2eccfbc97ee7
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-01-31 10:09:53 -0500

    Updated from Brython Server: 1/31/2018 10:09:52 AM

--- unitConverter.py
+++ unitConverter.py
@@ -2,4 +2,21 @@
 #1/30/18
 #unitConverter.py
 
+number=int(input('Pick a number from 1-4:'))
 
+
+if number==1:
+    kilometers=float(input('Enter number of kilometers'))
+    print(kilo,'is',kilo*0.621371,'mile')
+elif number==2:
+    kilogram=float(input('Enter number of kilograms'))
+    print(kilogram,'is',kilogram*2.20462,'llbs')
+elif number==3:
+    liter=float(input('Enter number of liter'))
+    print(liter,'is',liter*0.264172,'gallons')
+else:
+    celsius=float(input('Enter Degrees in Celsius')
+    print(celsius,'is',celsius*1.8+32,'celsius')
+
+    
+    
