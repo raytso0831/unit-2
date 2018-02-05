ce3164d12f0c43c4c31ba836530134589ce9324f
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-02-05 08:48:48 -0500

    Updated from Brython Server: 2/5/2018 8:48:47 AM

--- adventure.py
+++ adventure.py
@@ -3,4 +3,69 @@
 #program.py
 
 print('You are in the woods and a bear is chasing you')
-print('You come 
+print('You come  to a river')
+
+choice=input('Did you jump in the river:')
+
+if choice =='yes':
+    print('Thankfully you know how to swim')
+    print('However you see a shark')
+    choice_2=input('Did you punch the shark:')
+
+    if choice_2 =='yes':
+        print('The shark eats you and you die')
+    else:
+        print('The shark ignores you and you successfully cross the river')
+else:
+    print('The bear kills you')
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
