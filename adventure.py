1cd064c2b09e8e57902a1adb39702487ebc6d016
raytso0831 <35492837+raytso0831@users.noreply.github.com>
2018-02-06 20:19:39 -0500

    Updated from Brython Server: 2/6/2018 8:19:38 PM

--- adventure.py
+++ adventure.py
@@ -12,32 +12,35 @@ if choice =='yes':
     print('However you see a shark')
     choice_2=input('Did you punch the shark:')
 
-    if choice_3 =='yes':
-        print('You see a another bear on the bridge and you see a car ')
-        choice_3=input('Did you jump in the river:')
-        if choice_3 =='yes':
-            print('The shark eats you and you die')
-        else:
-            print('The shark ignores you and you successfully cross the river')   
+    if choice_2 =='yes':
+        print('The shark eats you and you die')
     else:
         print('The shark ignores you and you successfully cross the river')
+        print('You see a another bear on the bridge and you see a car ')
+        choice_3=input('Did you get in the car:')
+        if choice_3 =='yes':
+            print('Thankfully you know how to drive and you never see the bear again')
+        else:
+            print('You jump off the bridge and you die')   
+
+
 else:
     print('The bear kills you')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
